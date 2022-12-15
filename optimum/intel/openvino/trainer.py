599b621de85b1b291d6a67879dd89344f84cf94b
Vui Seng Chua <vui.seng.chua@intel.com>
2022-12-14 22:42:44 -0800

    Revert overriding of create_optimizer in OVTrainer

--- optimum/intel/openvino/trainer.py
+++ optimum/intel/openvino/trainer.py
@@ -521,47 +521,6 @@ class OVTrainer(Trainer):
         return TrainOutput(self.state.global_step, train_loss, metrics)
 
 
-    def create_optimizer(self):
-        """
-        Setup the optimizer.
-
-        We provide a reasonable default that works well. If you want to use something else, you can pass a tuple in the
-        Trainer's init through `optimizers`, or subclass and override this method in a subclass.
-        """
-        opt_model = self.model_wrapped if is_sagemaker_mp_enabled() else self.model
-
-        if self.optimizer is None:
-            score_params = []
-            for n, p in opt_model.named_parameters():
-                if not p.requires_grad:
-                    continue
-                if "importance" in n:
-                    score_params.append(p)
-            decay_parameters = get_parameter_names(opt_model, ALL_LAYERNORM_LAYERS)
-            decay_parameters = [name for name in decay_parameters if "bias" not in name]
-            decay_parameters = [name for name in decay_parameters if "importance" not in name]
-            optimizer_grouped_parameters = [
-                {
-                    "params": score_params,
-                    "weight_decay": 0.0,
-                    "lr": 0.01
-                },
-                {
-                    "params": [p for n, p in opt_model.named_parameters() if n in decay_parameters],
-                    "weight_decay": self.args.weight_decay,
-                },
-                {
-                    "params": [p for n, p in opt_model.named_parameters() if n not in decay_parameters and 'importance' not in n],
-                    "weight_decay": 0.0,
-                },
-            ]
-
-            optimizer_cls, optimizer_kwargs = Trainer.get_optimizer_cls_and_kwargs(self.args)
-            self.optimizer = optimizer_cls(optimizer_grouped_parameters, **optimizer_kwargs)
-
-        return self.optimizer
-
-
     def compute_distillation_loss(self, inputs, student_logits):
         with torch.no_grad():
             teacher_logits = self.teacher(**inputs)
